5937b15a8fe4d3ecd90438a297b3999278b1d5b5
sudan <sudan@douban.com>
2017-09-05 12:14:26 +0800

    refactor and add more APIs

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
 
-import os
-import re
+from setuptools import setup
 import sys
 
-from setuptools import setup
-
+if sys.version_info[0] != 3 or sys.version_info[1] < 5:
+    sys.exit('Sorry, only python 3.5 and above is supported.')
 
 setup(
-    name='cos_lib3',
-    version='0.0.9',
+    name='qcloud_cos3',
+    version='0.1.0',
     description='an unofficial sdk for qcloud cos',
-    author='hupeng',
-    author_email='hupeng@imudges.com',
-    url='https://github.com/imu-hupeng/cos-python3-sdk',
-    packages=['cos_lib3'],
+    author='su27',
+    author_email='damn.su@gmail.com',
+    url='https://github.com/su27/cos-python3-sdk',
+    packages=['qcloud_cos3'],
     include_package_data=True,
-    install_requires=['requests>=2.0','httplib2>=0.9'],
+    install_requires=['requests>=2.0'],
     license='MIT License',
     zip_safe=False,
 )
